3c23319ef37a1fe61a8c50560ddab483706873e1
MyungJin Eun <eunmyun@uw.edu>
2017-05-31 03:02:41 -0700

    Pie chart works. Need to be reusable

--- js/PieChart.js
+++ js/PieChart.js
@@ -13,10 +13,12 @@ $(function() {
     var drawHeight = height - margin.top - margin.bottom;
 
     var svg = d3.select("#attackTypesVis")
+                .append('svg')
                 .attr('width', width)
                 .attr('height', height);
 
-    radius = Math.min(width, height) / 2,
+    var radius = Math.min(width, height) / 2;
+    var donutwidth = radius /2; 
     g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var color = d3.scaleOrdinal(d3.schemeCategory20b);
@@ -27,13 +29,26 @@ $(function() {
         })
         .value(function(d) { return d.value; });
 
+    var tooltip = d3.select('#attackTypesVis')                               
+        .append('div')                                                
+        .attr('class', 'toooltip')
+        .style('width', donutwidth + "px")
+        .style('left', (drawWidth / 2) - 35 + "px")
+        .style('top', (drawHeight / 2) + "px");   
+                    
+    tooltip.append('p')
+        .attr('class', 'type');
+            
+    tooltip.append('p')
+        .attr('class', 'deathcount');
+
 
     var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
+        .outerRadius(radius - donutwidth)
+        .innerRadius(radius);
 
     var label = d3.arc()
-        .outerRadius(radius - 40)
+        .outerRadius(radius)
         .innerRadius(radius - 40);
 
     d3.csv("./data/globalterrorismdb_0616dist.csv", function(error, data) {
@@ -52,30 +67,23 @@ $(function() {
             .enter().append("g")
             .attr("class", "arc")
             
-            // .on("mouseover", function(d) {
-            //     //console.log(d);
-            //     //console.log(d3.selectAll(".tooltip")._groups[0]);
-            //     // d3.selectAll(".tooltip")[d.index].style('opacity', 1);
-            //         // .select("#value").text(d.data.value);
-            // })
-            // .on("mousemove", function(d) {
-            //     d3.selectAll(".tooltip").style("top", (d3.event.pageY - 10) + "px")
-            //     .style("left", (d3.event.pageX + 10) + "px");
-            // })
-            // .on("mouseout", function() {
-            //     d3.selectAll(".tooltip").style('opacity', 0);
-            // });
-
         arc.append("path")
+            .attr('class', 'paths')
             .attr("d", path)
-            .attr("fill", function(d) { console.log(d); return color(d.data.key); })
+            .attr("fill", function(d) { return color(d.data.key); })
 
-        arc.append("text")
-            .attr("class", "tooltip")
-            .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
-            .attr("dy", "0.35em")
-            .text(function(d) { console.log(d.data.key); return d.data.key; });
+            svg.selectAll('path').on('mouseover', function(d) {
+                tooltip.select('.type').html('<p>From<br /><strong>' + d.data.key + '</strong> type of attacks,</p>'); 
+                tooltip.select('.deathcount').html('<p><strong>' + Math.round(d.data.value) + '</strong> people died. </p>'); 
+                tooltip.style('display', 'block');
+                svg.selectAll('path').attr('opacity', 0.5);
+                event.srcElement.attributes.opacity.value = 1;
 
+            })
+            .on('mouseout', function() {
+                tooltip.style('display', 'none');
+                svg.selectAll('path').attr('opacity', 1)
+            });
         
      
     });
