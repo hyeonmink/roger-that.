b3b525150f656b48848a59799a70e4ca33ad58dc
Your Name <you@example.com>
2017-06-01 09:29:44 -0700

    Work on peoplechart

--- js/PeopleChart.js
+++ js/PeopleChart.js
@@ -1,111 +1,140 @@
 'use strict';
 $(function() {
-    var yearTarget;
-    var dim = {width: 960, height: 500};
-    var margin = {top: 10, bottom: 50, left: 50, right: 10};
-    var inputHeight = 20;
-    var numberFormat = d3.format('.0f');
-    dim.graphWidth = dim.width - margin.left - margin.right;
-    dim.graphHeight = dim.height - margin.top - margin.bottom;
+    d3.csv("./data/globalterrorismdb_0616dist.csv", function(error, data) {
+        var bar = BarChart()
+        var peopleChart = d3.select('#vis')
+            .datum(data)
 
-    var svg = d3.select('body').append('svg')
-        .attr('width', dim.width)
-        .attr('height', dim.height);
-    var axisLayer = svg.append('g').attr('transform','translate(' + margin.left + ',' + margin.top + ')');
-    var graphLayer = svg.append('g').attr('transform','translate(' + margin.left + ',' + margin.top + ')');
+        peopleChart.enter().append('div')
+            .attr('class', 'peopleChart')
+            .merge(peopleChart)
+            .call(bar);
+        
+        peopleChart.exit().remove();
+    });
 
-    var xScale = d3.scaleBand().range([0,dim.graphWidth],0.05);
-    var xLocalScale = d3.scaleBand();
-    var yScale = d3.scaleBand().range([dim.graphHeight, 0]);
-    var colorScale = d3.scaleOrdinal(d3.schemeCategory20b);
-    // var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    var BarChart = function() {
 
-    var xAxis = d3.axisBottom().scale(xScale);
-    var yAxis = d3.axisLeft().scale(yScale);
+        var dim = {width: 960, height: 500};
+        var margin = {top: 10, bottom: 50, left: 50, right: 10};
+        var inputHeight = 20;
+        var numberFormat = d3.format('.0f');
+        dim.graphWidth = dim.width - margin.left - margin.right;
+        dim.graphHeight = dim.height - margin.top - margin.bottom;
 
-    var xAxisObj = axisLayer.append('g')
-        .attr('transform','translate('+0+','+dim.graphHeight+')')
-        .attr('class','axis')
-        .call(xAxis);
-    var yAxisObj = axisLayer.append('g')
-        .attr('transform','translate('+0 +','+0+')')
-        .attr('class','axis')
-        .call(yAxis);
+        var chart = function(selection) {
+            var svg = d3.select('#vis').append('svg')
+                .attr('width', dim.width)
+                .attr('height', dim.height);
+            var axisLayer = svg.append('g').attr('transform','translate(' + margin.left + ',' + margin.top + ')');
+            var graphLayer = svg.append('g').attr('transform','translate(' + margin.left + ',' + margin.top + ')');
 
-    axisLayer.selectAll('.axis text').style('font','14px "Lucida Grande", Helvetica, Arial, sans-serif');
-    axisLayer.selectAll('.axis path.domain').style({fill:'none',stroke:'#000000','shape-rendering':'crispEdges'});
+            var xScale = d3.scaleBand().range([0,dim.graphWidth],0.05);
+            var xLocalScale = d3.scaleBand();
+            var yScale = d3.scaleBand().range([dim.graphHeight, 0]);
+            var colorScale = d3.scaleOrdinal(d3.schemeCategory20b);
 
-    var time = 0;
-    var yearLabel = 'year';
-    var radius = 3;
-    var mar = 0.6;
-    var barWidth = 16;
-    var displaydata = [];
-    var years = [];
+            var xAxis = d3.axisBottom().scale(xScale);
+            var yAxis = d3.axisLeft().scale(yScale);
 
-    d3.csv('../data/globalterrorismdb_0616dist.csv', function(error,datum) {
-            var raw = d3.nest()
+            var xAxisObj = axisLayer.append('g')
+                .attr('transform','translate('+0+','+dim.graphHeight+')')
+                .attr('class','axis')
+                .call(xAxis);
+            var yAxisObj = axisLayer.append('g')
+                .attr('transform','translate('+0 +','+0+')')
+                .attr('class','axis')
+                .call(yAxis);
+
+            axisLayer.selectAll('.axis text').style('font','14px "Lucida Grande", Helvetica, Arial, sans-serif');
+            axisLayer.selectAll('.axis path.domain').style({fill:'none',stroke:'#000000','shape-rendering':'crispEdges'});
+
+            var time = 0;
+            var yearLabel = 'year';
+            var radius = 1;
+            var mar = 0.6;
+            var barWidth = 16;
+            var displaydata = [];
+
+            var years2 = [];
+            var deaths = [];
+
+            selection.each(function(data) {
+                var raw = d3.nest()
                     .key(function(d) {
                         return (d.iyear).substring(0, 3);
                     })
                     .rollup(function(d) {
                         return d3.sum(d, function(v) { return Math.round(v.nkill); })
                     })
-                    .entries(datum);
+                    .entries(data);
             
-            raw.forEach(function(d) { d.key = (d.key).substring(2) + "0's" });
-            var years2 = raw.map(function(d) { return d.key });
-            var deaths = raw.map(function(d) { return d.value; });
-    if (error != null)
-    {
-        console.log(error);
-        return;
-    }
+                //raw.forEach(function(d) { d.key = (d.key).substring(2) + "0's" });
 
-    var parties = years2;
-    var partDict = {};
-    parties.forEach(function(d,i)
-    {
-        partDict[d] = i;
-    });
-    var sums = {};
-    var data = {};
+                raw.forEach(function(d) { years2.push((d.key).substring(2) + "0's"); });
+                raw.forEach(function(d) { deaths.push(d.value); });
 
-    data[0] = deaths;
+                
+            });
 
-    var max = d3.max(deaths);
-    var nrow = Math.ceil(dim.graphHeight/(2*(radius+mar)));
-    barWidth = Math.ceil(max/nrow);
-    yScale.domain(d3.range(nrow));
-    yAxis.tickValues(d3.range(nrow).filter(function(d){return d%10===0;}));
-    yAxis.tickFormat(function(d){return (d*barWidth);});
-    xScale.domain(parties.map(function(d,i){return i;}));
-    xAxis.tickFormat(function(d){return parties[d];});
-    xAxisObj.call(xAxis);
-    yAxisObj.call(yAxis);
-    xLocalScale.range([0,xScale.bandwidth()]).domain(d3.range(barWidth));
-    colorScale.domain(d3.range(parties.length));
 
-    var summax = d3.sum(deaths)
-    var displaydata = d3.range(summax).map(function(d){return [];});
+            var parties = years2;
+            var partDict = {};
+            parties.forEach(function(d,i) {
+                partDict[d] = i;
+            });
+            var sums = {};
+            var data = {};
 
-    var indexMargin = 0;
-    parties.forEach(function(party,partyidx)
-    {
-        for (var i=0;i<data[0][partyidx];++i)
-        {
-        displaydata[indexMargin+i].push({label:partyidx,idx:i});
+            data[0] = deaths;
+
+            var max = d3.max(deaths);
+            var nrow = Math.ceil(dim.graphHeight/(2*(radius+mar)));
+            barWidth = Math.ceil(max/nrow);
+            yScale.domain(d3.range(nrow));
+            yAxis.tickValues(d3.range(nrow).filter(function(d){return d%10===0;}));
+            yAxis.tickFormat(function(d){return (d*barWidth);});
+            xScale.domain(parties.map(function(d,i){return i;}));
+            xAxis.tickFormat(function(d){return parties[d];});
+            xAxisObj.call(xAxis);
+            yAxisObj.call(yAxis);
+            xLocalScale.range([0,xScale.bandwidth()]).domain(d3.range(barWidth));
+            colorScale.domain(d3.range(parties.length));
+
+            var summax = d3.sum(deaths)
+
+            var displaydata = d3.range(summax).map(function(d){return [];});
+
+            var indexMargin = 0;
+            parties.forEach(function(party,partyidx)
+            {
+
+                for (var i=0;i<data[0][partyidx];++i)
+                {
+                displaydata[indexMargin+i].push({label:partyidx,idx:i});
+                }
+                indexMargin += data[0][partyidx];
+            });
+
+
+            var votes = graphLayer.selectAll('.vote').data(displaydata)
+
+            votes.enter().append('circle')
+                .attr('class','vote')
+                .attr('r',radius)
+                // .attr('cx', 10)
+                // .attr('cy', 10)
+                // .transition()
+                // .duration(1500)
+                // .delay(4000)
+                .attr('cx',function(d){ return ((d[time].label!=null)?(xScale(d[time].label)+xLocalScale(d[time].idx%barWidth)+radius+mar):(dim.graphWidth/2)); })
+                .attr('cy',function(d){return ((d[time].label!=null)?(yScale(Math.floor((d[time].idx+0.1)/barWidth))-radius-mar):0);})
+                .style('opacity',function(d){return (d[time].label!=null)?0.8:0.0;})
+                .style('fill',function(d){return colorScale(d[time].label);});
+
+            votes.exit().remove();
+            
         }
-        indexMargin += data[0][partyidx];
-    });
-
-
-    var votes = graphLayer.selectAll('.vote').data(displaydata).enter().append('circle')
-        .attr('class','vote')
-        .attr('r',radius)
-        .attr('cx',function(d){ return ((d[time].label!=null)?(xScale(d[time].label)+xLocalScale(d[time].idx%barWidth)+radius+mar):(dim.graphWidth/2)); })
-        .attr('cy',function(d){return ((d[time].label!=null)?(yScale(Math.floor((d[time].idx+0.1)/barWidth))-radius-mar):0);})
-        .style('opacity',function(d){return (d[time].label!=null)?0.8:0.0;})
-        .style('fill',function(d){return colorScale(d[time].label);});
-    });
+        return chart;
+    }
 });
